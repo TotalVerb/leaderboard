37b734ce2437d27695d9bc07f67f71b7262d0bb0
Fengyang Wang <fengyang.wang.0@gmail.com>
2018-12-07 01:41:23 -0500

    basic theScore stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,173 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
 
-import logo from './logo.svg';
+import logo from './logo.svg'
+import { HTMLTable } from "@blueprintjs/core"
+import { playerNumbers } from "./theScoreNumber"
 
-class App extends React.Component {
+interface Athlete {
+  name: string
+  position: number
+  fpts: number
+  team: string
+  points: number
+  assists: number
+  rebounds: number
+  steals: number
+  blocks: number
+  turnovers: number
+  color: string
+  projection: number
+  three_point_made: number
+}
+
+interface LineupMap {
+  [name: string]: Array<Athlete>
+}
+
+interface Contestant {
+  lineup: Array<Athlete>
+  name: string
+  total: number
+}
+
+function Contestant(lineups: LineupMap, contestant: string): Contestant {
+  return {
+    lineup: lineups[contestant],
+    name: contestant,
+    total: lineups[contestant].map(data => data.fpts).reduce((f, g) => f + g, 0.0),
+  }
+}
+
+interface AthleteProps {
+  athlete: Athlete
+}
+
+function getHeadshot(number: number) {
+  return `https://d1si3tbndbzwz9.cloudfront.net/basketball/player/${number}/transparent_medium_headshot.png`
+}
+
+function AthleteCell({athlete}: AthleteProps) {
+  let theScoreNumber = playerNumbers[athlete.name]
+  let [red, green, blue] = [
+    parseInt(athlete.color.slice(1, 3), 16),
+    parseInt(athlete.color.slice(3, 5), 16),
+    parseInt(athlete.color.slice(5, 7), 16),
+  ]
+  let style = {
+    backgroundColor: `rgba(${red}, ${green}, ${blue}, 0.5)`,
+  }
+  return (
+    <>
+      <td className="athlete-name" style={style}>
+        <b>{athlete.name}</b>
+        {theScoreNumber !== undefined ?
+          (<img src={getHeadshot(theScoreNumber)} className="headshot" />) :
+          null}
+      </td>
+      <td className="score">{athlete.fpts.toFixed(1)}</td>
+    </>
+  )
+}
+
+interface ScoreboardRowProps {
+  contestant: Contestant
+}
+
+function ScoreboardRow({contestant}: ScoreboardRowProps) {
+  return (
+    <tr>
+      <td>{contestant.name}</td>
+      <td>{contestant.total.toFixed(1)}</td>
+      { contestant.lineup.map(athlete => (<AthleteCell athlete={athlete} />)) }
+    </tr>
+  )
+}
+
+class ScoreboardHeader extends React.Component {
+  public render() {
+    return (
+      <thead>
+        <th>Name</th>
+        <th>Score</th>
+        <th colSpan={10}>Lineup</th>
+      </thead>
+    )
+  }
+}
+
+interface ScoreboardBodyProps {
+  lineups: LineupMap
+}
+
+class ScoreboardBody extends React.Component<ScoreboardBodyProps, {}> {
+  public render() {
+    let rows = Object.keys(this.props.lineups).map(
+      contestant => (
+        <ScoreboardRow
+          contestant={Contestant(this.props.lineups, contestant)}
+          key={contestant}
+        />)
+    )
+    return (
+      <tbody>
+        {rows}
+      </tbody>
+    )
+  }
+}
+
+interface ScoreboardProps {
+  lineups: LineupMap
+}
+
+class Scoreboard extends React.Component<ScoreboardProps, {}> {
+  public render() {
+    return (
+      <HTMLTable bordered={true}>
+        <ScoreboardHeader />
+        <ScoreboardBody lineups={this.props.lineups} />
+      </HTMLTable>
+    )
+  }
+}
+
+interface AppState {
+  data: LineupMap
+}
+
+class App extends React.Component<{}, AppState> {
+  public constructor(props: {}) {
+    super(props)
+    this.state = {
+      data: {}
+    }
+  }
+
+  public refreshScoreboard = () => {
+    fetch('https://nba.uwseminars.com/api')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ data: response })
+      })
+  }
+
+  public componentDidMount() {
+    this.refreshScoreboard()
+    setInterval(this.refreshScoreboard, 30000)
+  }
+
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Fantasy NBA Live Scoreboard</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Scoreboard lineups={this.state.data} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import 'normalize.css/normalize.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
 
 ReactDOM.render(
   <App />,
